a456a62bc013e102d0723ccaa75f2faae86da7e9
ritaaadr <49071121+ritaaadr@users.noreply.github.com>
2025-06-05 14:58:34 +0200

    Update finetune_wavlm_large_linear_vicuna_7b.sh

--- examples/asr_librispeech/scripts/finetune_wavlm_large_linear_vicuna_7b.sh
+++ examples/asr_librispeech/scripts/finetune_wavlm_large_linear_vicuna_7b.sh
@@ -17,8 +17,10 @@ code_dir=examples/asr_librispeech
 
 speech_encoder_path=/workspace/SLAM-LLM/checkpoints/WavLM-Large.pt
 llm_path=/workspace/SLAM-LLM/src/slam_llm/models/vicuna-7b
-train_data_path=/workspace/SLAM-LLM/LibriSpeech/debug-wav-fixed.jsonl
-val_data_path=/workspace/SLAM-LLM/LibriSpeech/librispeech_dev.jsonl
+#train_data_path=/workspace/SLAM-LLM/LibriSpeech/jsonl/debug-wav-fixed.jsonl
+#val_data_path=/workspace/SLAM-LLM/LibriSpeech/jsonl/debug-wav-fixed.jsonl
+train_data_path=/workspace/SLAM-LLM/dataset/jsonl/training.jsonl
+val_data_path=/workspace/SLAM-LLM/dataset/jsonl/validation.jsonl
 
 output_dir=/workspace/SLAM-LLM/tmp/vicuna-7b-v1.5-librispeech-linear-steplrwarmupkeep1e-4-wavlm-large-$(date +"%Y%m%d")
 
@@ -34,22 +36,27 @@ hydra.run.dir=$output_dir \
 ++model_config.encoder_path=$speech_encoder_path \
 ++model_config.encoder_dim=1024 \
 ++model_config.encoder_projector=linear \
+++model_config.load_in_8bit=true \
 ++dataset_config.dataset=speech_dataset \
 ++dataset_config.train_data_path=$train_data_path \
 ++dataset_config.val_data_path=$val_data_path \
 ++dataset_config.input_type=raw \
 ++train_config.model_name=asr \
-++train_config.num_epochs=1 \
+++train_config.num_epochs=3 \
 ++train_config.freeze_encoder=true \
 ++train_config.freeze_llm=true \
 ++train_config.batching_strategy=custom \
-++train_config.warmup_steps=1000 \
-++train_config.total_steps=100000 \
-++train_config.lr=1e-4 \
-++train_config.validation_interval=1000 \
-++train_config.batch_size_training=1 \
+++train_config.context_length=2048 \
+++train_config.warmup_steps=15 \
+++train_config.total_steps=600 \
+++train_config.lr=1e-2 \
+++train_config.use_gradient_checkpointing=true \
+++train_config.validation_interval=400 \
+++train_config.batch_size_training=3 \
 ++train_config.val_batch_size=1 \
 ++train_config.num_workers_dataloader=2 \
+++train_config.low_cpu_fsdp=false \
+++train_config.quantization=true \
 ++train_config.output_dir=$output_dir \
 ++metric=acc \
 "
